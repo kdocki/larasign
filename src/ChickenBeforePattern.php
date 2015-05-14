0e9564a2df09273511832b06556b7867bfb920b3
Kelt Dockins <kelt@dockins.org>
2015-05-14 10:20:12 -0500

    simplifying this class

--- src/ChickenBeforePattern.php
+++ src/ChickenBeforePattern.php
@@ -9,17 +9,7 @@ class Chicken
 
 	public function speaks()
 	{
-		print $this->makeNoise() . PHP_EOL;
-	}
-
-	public function scratch()
-	{
-		print 'scratches some dirt' . PHP_EOL;
-	}
-
-	protected function makeNoise()
-	{
-		switch ($this->type)
+		switch ($this->noisetype)
 		{
 			case 'hen': 	return 'cluck, cluck';
 			case 'chick': 	return 'chirp, chirp';
@@ -29,5 +19,10 @@ class Chicken
 		}
 
 		return '';
+	}
+
+	public function scratch()
+	{
+		print 'scratches some dirt' . PHP_EOL;
 	}
 }
